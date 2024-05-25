37956ca90579e015db0619a2c8009cca89a5b72b
Izanna <idanielewska@interia.pl>
2024-05-25 18:36:26 +0200

    up szybszy

--- R/neighbours.R
+++ R/neighbours.R
@@ -1,12 +1,6 @@
 neighbours <- function(nei, state) {
-  nb_minus <- 0
-  nb_plus <- 0
-  for (i in state[as.logical(nei)]){
-    if (i==1){
-      nb_plus <- nb_plus + 1
-    } else{
-      nb_minus <- nb_minus + 1
-    }
-  }
+  tmp <- state[as.logical(nei)]
+  nb_plus <- sum(tmp == 1)
+  nb_minus <- length(tmp) - nb_plus
   list(nb_minus = nb_minus, nb_plus = nb_plus)
 }
