0ce875e48426499e1399674c3de58e2f3c0b2386
Izanna <idanielewska@interia.pl>
2024-05-31 00:57:07 +0200

    up wyniki

--- R/wiz_graf.R
+++ R/wiz_graf.R
@@ -1,53 +1,87 @@
 library(igraph)
-edges <- c(1, 2, 2, 3, 3, 4, 4, 1, 2, 4)
-g <- graph(edges, directed=FALSE)
-plot(g, main="Simple Graph")
-
-# Define vertices and edges
-vertices <- c(1, 2, 3, 4)
-edges <- c(1, 2, 2, 3, 3, 4, 4, 1, 2, 4)
-
-# Create graph object
-g <- graph(edges, directed=FALSE)
-
-# Set vertex attributes
-V(g)$name <- c("A", "B", "C", "D")
-V(g)$color <- c("red", "green", "blue", "yellow")
-V(g)$size <- c(15, 30, 45, 60)
-
-# Set edge attributes
-E(g)$color <- "gray"
-E(g)$width <- 2
-
-# Plot the graph
-plot(g, vertex.label=V(g)$name, vertex.color=V(g)$color, vertex.size=V(g)$size,
-     edge.color=E(g)$color, edge.width=E(g)$width, main="Graph with Attributes")
 
 vertices <- 1:6
 edges <- unlist(complete.graph.gen(6)$edges)
 g <- graph(edges, directed=FALSE)
-plot(g, vertex.color = (ising(0.01, complete.graph.gen(6))+1), vertex.size = 50, vertex.label = NA)
-
-vertices <- 1:6
-edges <- unlist(complete.graph.gen(6)$edges)
-g <- graph(edges, directed=FALSE)
-temp <- seq(0, 1, by = 0.01)
+temp <- c(0, 0.01, 0.1, 0.4, 0.5, 0.8)
 for (t in temp) {
-  file <- paste0("~/GitHub/LM/R/Wygenerowane obrazki/tree_graph", t*100, ".png")
-  png(file, width = 800, height = 600)
-  plot(g, vertex.color = (ising(t, complete.graph.gen(6))+3), vertex.size = 50, vertex.label = NA)
-  dev.off()
+  for (i in 1:5) {
+    file <- paste0("~/GitHub/LM/R/Wygenerowane obrazki/complete_graph", t*100, i, ".png")
+    png(file, width = 800, height = 600)
+    plot(g, vertex.color = (ising(t, complete.graph.gen(6))+3), vertex.size = 50, vertex.label = NA)
+    dev.off()
+  }
 }
 
 vertices <- 1:16
 edges <- unlist(lattice.gen(4,4)$edges)
 g <- graph(edges, directed=FALSE)
-temp <- seq(0, 1, by = 0.01)
+temp <- c(0, 0.01, 0.1, 0.4, 0.5, 0.8)
 for (t in temp) {
-  file <- paste0("~/GitHub/LM/R/Wygenerowane obrazki/lattice_graph", t*100, ".png")
-  png(file, width = 800, height = 600)
-  plot(g, vertex.color = (ising(t, lattice.gen(4,4))+3), vertex.size = 50, vertex.label = NA)
-  dev.off()
+  for (i in 1:5){
+    file <- paste0("~/GitHub/LM/R/Wygenerowane obrazki/lattice_graph", t*100, i, ".png")
+    png(file, width = 800, height = 600)
+    plot(g, vertex.color = (ising(t, lattice.gen(4,4))+3), vertex.size = 30, vertex.label = NA)
+    dev.off()
+  }
 }
 
+vertices <- 1:10
+edges <- unlist(tree.gen(10, 3)$edges)
+g <- graph(edges, directed=FALSE)
+temp <- c(0, 0.01, 0.1, 0.4, 0.5, 0.8)
+for (t in temp) {
+  for (i in 1:5){
+    file <- paste0("~/GitHub/LM/R/Wygenerowane obrazki/tree_graph", t*100, i, ".png")
+    png(file, width = 800, height = 600)
+    plot(g, vertex.color = (ising(t, tree.gen(10, 3))+3), vertex.size = 25, vertex.label = NA)
+    dev.off()
+  }
+}
+
+
+library("microbenchmark")
+result_complete <- list()
+for (n in 2:7) {
+  result_complete[[length(result_complete)+1]] <- microbenchmark(ising(0.4, complete.graph.gen(n)), times = 10, unit = "s")
+}
+
+result_tree <- list()
+for (n in 2:20) {
+  result_tree[[length(result_tree)+1]] <- microbenchmark(ising(0.4, tree.gen(n)), times = 10, unit = "s")
+}
+
+result_lattice <- list()
+for (i in 2:5) {
+  for (j in 2:5){
+    result_lattice[[length(result_lattice)+1]] <- microbenchmark(ising(0.4, lattice.gen(i, j)), times = 10, unit = "s")
+  }
+}
+
+save(result_complete, file = "~/GitHub/LM/R/Wyniki czasowe/complete_graph.RData")
+save(result_tree, file = "~/GitHub/LM/R/Wyniki czasowe/tree_graph.RData")
+save(result_lattice, file = "~/GitHub/LM/R/Wyniki czasowe/lattice_graph.RData")
+
+temp <- c(0, 0.01, 0.1, 0.4, 0.8, 1)
+result_complete_temp <- list()
+for (t in temp) {
+  result_complete_temp[[length(result_complete_temp)+1]] <- microbenchmark(ising(t, complete.graph.gen(5)), times = 10, unit = "s")
+  print(t)
+}
+
+result_tree_temp <- list()
+for (t in temp) {
+  result_tree_temp[[length(result_tree_temp)+1]] <- microbenchmark(ising(t, tree.gen(8)), times = 10, unit = "s")
+  print(t)
+}
+
+result_lattice_temp <- list()
+for (t in temp) {
+  result_lattice_temp[[length(result_lattice_temp)+1]] <- microbenchmark(ising(t, lattice.gen(3, 3)), times = 10, unit = "s")
+  print(t)
+}
+
+save(result_complete_temp, file = "~/GitHub/LM/R/Wyniki czasowe/complete_graph_temp.RData")
+save(result_tree_temp, file = "~/GitHub/LM/R/Wyniki czasowe/tree_graph_temp.RData")
+save(result_lattice_temp, file = "~/GitHub/LM/R/Wyniki czasowe/lattice_graph_temp.RData")
 
